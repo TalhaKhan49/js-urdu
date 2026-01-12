4ba521f56e302d3f44dca8699464f07288e01770
Muhammad Talha Khan <114686314+TalhaKhan49@users.noreply.github.com>
2026-01-12 18:29:45 +0000

    3rd file

---
+++ .devcontainer/01_basics/03_conversion_operation.js
@@ -0,0 +1,15 @@
+// let score = true
+// console.log(typeof score)
+
+// let me = Number(score)
+// console.log(typeof me);
+// console.log(me);
+
+//33 => number
+//"33abc" => NaN
+//true => 1, false => 0;
+
+let myNumber = 23;
+ let me = String(myNumber)
+ console.log(typeof me);
+ 
